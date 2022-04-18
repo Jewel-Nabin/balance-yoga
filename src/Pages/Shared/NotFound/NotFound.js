e4493785d8622f35888ee614a1e6cd0c573bba65
Mohammed Jewel <jewelnabin@gmail.com>
2022-04-18 12:45:41 +0600

    Not Page Found

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import sleeping from '../../../images/sleeping.jpg';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 
 const NotFound = () => {
     return (
-        <div>
-            <div>
-                <h2 className='text-primary text-center'>Mechanic is sleeping</h2>
-                <img className='w-100' src={sleeping} alt="" />
-            </div>
+        <div className='container mt-5 text-center text-secondary not-page-found'>
+            <h1>Oops!</h1>
+            <hr />
+            <p>I love to simplify and it looks like this page has been decluttered!</p>
+            <p>(Decluttering is good for the mind and soul).</p>
+            <Link to={'/home'}>
+                <button className='not-found-btn rounded not-found-btn'>Back to Home</button>
+            </Link>
         </div>
     );
 };
